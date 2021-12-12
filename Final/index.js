cb7c5efd377aa0d8df0e6d0bc3bc8d0a00043551
tXenokrates <89860612+tXenokrates@users.noreply.github.com>
2021-12-11 22:32:46 -0800

    Final Project!

---
+++ Final/index.js
@@ -0,0 +1,29 @@
+document.getElementById("EuropaButton").addEventListener("click", GoToAboutMe);
+  function GoToAboutMe(){
+    window.location.href = "\AboutMe.html";
+    }
+
+document.getElementById("MoonButton").addEventListener("click", GoToScience);
+  function GoToScience(){
+    window.location.href = "\Science.html";
+    }
+
+document.getElementById("EnceladusButton").addEventListener("click", GoToPhotos);
+  function GoToPhotos(){
+    window.location.href = "\Photos.html";
+    }
+
+document.getElementById("HomeButton").addEventListener("click", GoToHome);
+  function GoToHome(){
+      window.location.href = "\index.html";
+    }
+
+
+// Image Citations (for images from the internet):
+
+// Cover Image of Mars: https://wallpaperaccess.com/full/1608.jpg
+// Titan button image: https://static.wikia.nocookie.net/thesolarsystem6361/images/0/04/Titan_spacepedia.png/revision/latest/scale-to-width-down/250?cb=20180302104831
+// Europa button image: https://static.wikia.nocookie.net/thesolarsystem6361/images/a/ab/Europa_spacepedia.png/revision/latest?cb=20180301165224
+// Enceladus button image: https://static.wikia.nocookie.net/thesolarsystem6361/images/8/86/Enceladus_spacepedia.png/revision/latest?cb=20180302104450
+// Mare Orientale image on science page: https://upload.wikimedia.org/wikipedia/commons/2/26/Mare_Orientale_%28Lunar_Orbiter_4%29.png
+// The rest of the images are my own (the NASA internship headshot and my photography)
